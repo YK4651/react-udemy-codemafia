e09256d1ed995bbc529d86f7cc48de21b3e9c861
YK4651 <yoshik4651@gmail.com>
2025-01-03 21:42:46 -0800

    04-075-fragment

--- 04_react_basic/src/075_fragment/start/components/Child.jsx
+++ 04_react_basic/src/075_fragment/start/components/Child.jsx
@@ -1,10 +1,15 @@
 import "./Child.css";
+import react from 'react'
 
 const Child = () => {
   return (
+    <react.Fragment>
     <div className="component">
       <h3>Hello Component</h3>
     </div>
+    <h3>Hello Fragment</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae quidem doloribus nobis harum, expedita sit rerum repellendus quibusdam omnis, exercitationem ratione distinctio id. Maxime eum aliquid aliquam veniam impedit sint!</p>
+    </react.Fragment>
   );
 };
 
