be091b896b89cb3f556c4b3ba684fdedffa36e8e
YK4651 <yoshik4651@gmail.com>
2025-01-13 18:24:28 -0800

    aa

--- 06_control_and_form/src/010_list_components/start/Example.jsx
+++ 06_control_and_form/src/010_list_components/start/Example.jsx
@@ -1,13 +1,19 @@
-
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
-
+  // const animalList = [];
+  // for (const animal of animals) {
+  //   animalList.push(<li>{animal}</li>);
+  // }
   return (
     <>
       <h3>配列の操作</h3>
+      {/* <ul>{animalList}</ul> */}
+      <br />
       <ul>
-        
+        {animals.map((animal) => (
+          <li key={animal}>{animal}</li>
+        ))}
       </ul>
     </>
   );
--- 06_control_and_form/src/040_list_and_filter/start/Example.jsx
+++ 06_control_and_form/src/040_list_and_filter/start/Example.jsx
@@ -3,12 +3,12 @@ import { useState } from "react";
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  const [filterVal, setFilter] = useState("");
   return (
     <>
       <h3>配列のフィルター</h3>
       <ul>
-        {animals
-          .map((animal) => (
+        {animals.map((animal) => (
           <li>{animal}</li>
         ))}
       </ul>
