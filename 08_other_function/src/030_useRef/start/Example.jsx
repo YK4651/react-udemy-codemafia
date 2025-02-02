f8b7d41dd53481e6b67455a517c235c88abedeb1
YK4651 <yoshik4651@gmail.com>
2025-02-01 23:47:51 -0800

    useRef

--- 08_other_function/src/030_useRef/start/Example.jsx
+++ 08_other_function/src/030_useRef/start/Example.jsx
@@ -1,14 +1,44 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Case1 = () => {
   const [value, setValue] = useState("");
+  const inputRef = useRef();
 
   return (
     <div>
       <h3>ユースケース1</h3>
-      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick>
+      <input
+        type="text"
+        value={value}
+        ref={inputRef}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={() => inputRef.current.focus()}>
         インプット要素をフォーカスする
+      </button>
+    </div>
+  );
+};
+
+const Case2 = () => {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef();
+  return (
+    <div>
+      <video style={{ maxWidth: "100%" }} ref={videoRef}>
+        <source src="./sample.mp4"></source>
+      </video>
+      <button
+        onClick={() => {
+          setPlaying((prev) => !prev);
+          if (playing === false) {
+            videoRef.current.play();
+          } else {
+            videoRef.current.pause();
+          }
+        }}
+      >
+        {playing ? "Stop" : "Play"}
       </button>
     </div>
   );
@@ -18,6 +48,7 @@ const Example = () => {
   return (
     <>
       <Case1 />
+      <Case2 />
     </>
   );
 };
--- 08_other_function/src/040_forwardRef/start/Example.jsx
+++ 08_other_function/src/040_forwardRef/start/Example.jsx
@@ -1,10 +1,15 @@
 import { useRef, forwardRef } from "react";
 
+// eslint-disable-next-line react/display-name
+const Input = forwardRef((props, ref) => {
+  return <input type="text" ref={ref} />;
+});
+
 const Example = () => {
   const ref = useRef();
   return (
     <>
-      <input type="text" ref={ref} />
+      <Input ref={ref} />
       <button onClick={() => ref.current.focus()}>
         インプット要素をフォーカスする
       </button>
