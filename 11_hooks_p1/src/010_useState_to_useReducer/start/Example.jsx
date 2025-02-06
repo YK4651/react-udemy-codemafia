e0bf16ac2b1e3c645da5e52c7a6d87637052798a
YK4651 <yoshik4651@gmail.com>
2025-02-06 03:24:36 -0800

    reducer

--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
@@ -1,7 +1,36 @@
+import { useReducer } from "react";
+import { useState } from "react";
+
 const Example = () => {
-  
+  const [state, setState] = useState(0);
+  const [rstate, dispatch] = useReducer((prev, { type, step }) => {
+    switch (type) {
+      case "+":
+        return prev + step;
+      case "-":
+        return prev - step;
+      default:
+        throw new Error("ERROR");
+    }
+  }, 0);
+  const countUp = () => {
+    setState((prev) => ++prev);
+  };
+  const rcountUp = () => {
+    dispatch({ type: "+", step: 2 });
+  };
+  const rcountDown = () => {
+    dispatch({ type: "-", step: 3 });
+  };
   return (
     <>
+      <h2>useState</h2>
+      <h3>{state}</h3>
+      <button onClick={countUp}>+</button>
+      <h2>useReducer</h2>
+      <h3>{rstate}</h3>
+      <button onClick={rcountUp}>+</button>
+      <button onClick={rcountDown}>-</button>
     </>
   );
 };
