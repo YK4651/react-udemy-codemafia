6f7b1e7bff25ec6f5a1a30955276253568e8624c
YK4651 <yoshik4651@gmail.com>
2025-02-15 22:50:46 -0800

    a

--- 11_hooks_p1/src/030_useContext/start/components/GrandChild.jsx
+++ 11_hooks_p1/src/030_useContext/start/components/GrandChild.jsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
+import { MyContext } from "../Example";
+
 const GrandChild = ({ value }) => {
+  const contextValue = useContext(MyContext);
   return (
-      <div style={{ border: "1px solid black" }}>
-        <h3>孫コンポーネント</h3>
-        {value}
-      </div>
+    <div style={{ border: "1px solid black" }}>
+      <h3>孫コンポーネント</h3>
+      {/* {value} */}
+      {contextValue}
+    </div>
   );
 };
 export default GrandChild;
