8c3ab8bf91e54738232947d857eb94520fee0855
YK4651 <yoshik4651@gmail.com>
2025-02-26 21:58:46 -0800

    a

--- 20_typescript/src/160_add_list_item/start/Example.tsx
+++ 20_typescript/src/160_add_list_item/start/Example.tsx
@@ -1,7 +1,28 @@
 import { useState } from "react";
 
+type Todo = {
+    name: string,
+    id: number
+}
+
 const Example = () => {
-  
+    const [ input, setInput ] = useState("");
+    const [ todo, setTodo ] = useState<Todo[]>([{name: "Todo1", id: 1}, {name: "Todo2", id: 2}]);
+
+    const addTodo = () => {
+        
+    }
+  return(
+    <>
+    <input type="text" value={input} onChange={(e)=> {setInput(e.target.value)}}></input>
+    <button onClick={addTodo}>
+        追加
+    </button>
+    <ul>
+        <li>{todo}</li>
+    </ul>
+    </>
+  )
 };
 
 export default Example;
